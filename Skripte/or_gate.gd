c7181550f6762a9ffe8750934b56f460a9cbffa6
svhu0511 <sven.huhnstock@mailbox.tu-dresden.de>
2026-06-04 14:13:11 +0200

    first commmit

---
+++ Skripte/or_gate.gd
@@ -0,0 +1,13 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@export var gate_type = "OR"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
