0ef01718445596dfb0adad00ef8e982ecfba64a0
SvHu0511 <sven.huhnstock@mailbox.tu-dresden.de>
2026-06-07 08:55:21 +0200

    Clean Up and Drag and Drop

--- Skripte/and_gate_new.gd
+++ Skripte/and_gate_new.gd
@@ -1,27 +1,23 @@
-extends Panel
+extends Area2D
 
+@export var gate_type = "AND"
+var dragging = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-@export var gate_type = "AND"
-
-var dragging = false
-var ofset = Vector2(0,0)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta) -> void:
+func _process(delta: float) -> void:
 	if dragging:
-		position = get_global_mouse_position() - ofset
-
-
-func _on_button_button_up() -> void:
-	dragging = false
+		position = get_global_mouse_position()
 
 
 func _on_button_button_down() -> void:
 	print("clicked")
 	dragging = true
-	ofset = get_global_mouse_position()
+
+
+func _on_button_button_up() -> void:
+	dragging = false
---
+++ Skripte/or_gate_new.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var gate_type = "OR"
+var dragging = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if dragging:
+		position = get_global_mouse_position()
+
+func _on_button_button_down() -> void:
+	print("clicked")
+	dragging = true
+
+
+func _on_button_button_up() -> void:
+	dragging = false
