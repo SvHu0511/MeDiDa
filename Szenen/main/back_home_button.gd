72b64adabdc4a3c5a5ced874027a6c08b270f0b7
tia-ko <tia.koblischke@gmail.com>
2026-06-23 01:11:01 +0200

    added back home button, changed start scene

--- Szenen/main/back_home_button.gd
+++ Szenen/main/back_home_button.gd
@@ -1,4 +1,4 @@
-extends Button
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_pressed():
-	get_tree().change_scene_to_file("res://Szenen/main/main.tscn")
-
-
-func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Szenen/main/home.tscn")
