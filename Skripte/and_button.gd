f628cd3097525494aa15eb1a554299050a978b7c
Matas Motiejunas <matas.motiejunas@mailbox.tu-dresden.de>
2026-06-25 09:38:55 +0200

    Gatter spawn position

--- Skripte/and_button.gd
+++ Skripte/and_button.gd
@@ -9,6 +9,6 @@ func _ready() -> void:
 func on_pressed():
 	print("pressed AND")
 	var instance = and_gate_scene.instantiate()
-	instance.position = Vector2(100, 100)
+	instance.position = Vector2(1500, 1300)
 	get_tree().current_scene.add_child(instance)
 	print("done creating")
