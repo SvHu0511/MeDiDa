7f473c4823fd78da1e1d37cfbae3f314f7c659c7
Matas Motiejunas <matas.motiejunas@mailbox.tu-dresden.de>
2026-07-03 20:34:45 +0200

    Buttons

--- Skripte/button_level_1.gd
+++ Skripte/button_level_1.gd
@@ -17,3 +17,6 @@ func _on_pressed():
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Szenen/main/home.tscn")
+
+func on_level1_pressed():
+	get_tree().change_scene_to_file("res://Szenen/main/level1.tscn")
