04a626ca89c3fe22f3f4e5a6d8858c9c196664ec
Matas Motiejunas <matas.motiejunas@mailbox.tu-dresden.de>
2026-07-04 18:25:32 +0200

    Debug verbessert

--- Skripte/fixed_target.gd
+++ Skripte/fixed_target.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var solved : bool
 @onready var root_node = self.owner
 @export var all_occupied = true
-
+@export var showing_debug = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -37,9 +37,32 @@ func check_solution_button():
 
 
 func debug_mode():
-	if all_occupied:
+	if not showing_debug:
+		if all_occupied:
+			#add labels
+			for slot in root_node.get_node("GateSlots").get_children():
+				var output_label = Label.new()
+				output_label.name = slot.name + " debug"
+				output_label.text = "1" if slot.output == true else "0"
+				var my_font = load("res://Assets/Watermelon Sugar.ttf")
+				output_label.add_theme_font_override("font", my_font)
+				output_label.add_theme_font_size_override("font_size", 50)
+				output_label.add_theme_color_override("font_color", "yellow")
+				output_label.position = slot.position + Vector2(100, -50)
+				root_node.add_child(output_label)
+			
+			showing_debug = true
+		else:
+			root_node.get_node("FillAll").visible = true;
+			await get_tree().create_timer(3).timeout
+			root_node.get_node("FillAll").visible = false;
+		
+		
+	else:
+		#delete labels
 		for slot in root_node.get_node("GateSlots").get_children():
-			var output_label = Label.new()
-			output_label.text = str(slot.output)
-			output_label.position = slot.position + Vector2(100, -50)
-			root_node.add_child(output_label)
+			var name = slot.name + " debug"
+			var label = root_node.get_node_or_null(name)
+			if label:
+				label.queue_free()
+		showing_debug = false
